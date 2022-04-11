5771cf6c1abf004c79a37133177227fa1f327009
沐萱 <fuqing.jfq@dtwave-inc.com>
2022-04-11 14:40:52 +0800

    project create

---
+++ src/main/java/com/muxuan/example/muxuan/test/MuxuanTestApplication.java
@@ -0,0 +1,13 @@
+package com.muxuan.example.muxuan.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuxuanTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuxuanTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muxuan/example/muxuan/test/MuxuanTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muxuan.example.muxuan.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuxuanTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
